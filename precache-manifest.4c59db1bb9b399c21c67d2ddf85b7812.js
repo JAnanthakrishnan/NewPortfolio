9ce2b5877f4034f11a097eca016af8ea2ca1e4f6
J Ananthakrishnan <ananthakrishnan2017@gmail.com>
2020-08-15 04:31:14 +0530

    Updates

---
+++ precache-manifest.4c59db1bb9b399c21c67d2ddf85b7812.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358a72d5b4cbf8f327d79088958f6dbb",
+    "url": "/NewPortfolio/index.html"
+  },
+  {
+    "revision": "00e615aa2a725eacb28c",
+    "url": "/NewPortfolio/static/css/main.62670f6f.chunk.css"
+  },
+  {
+    "revision": "98dfe5411308e0f7cb55",
+    "url": "/NewPortfolio/static/js/2.f10e2df9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NewPortfolio/static/js/2.f10e2df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e615aa2a725eacb28c",
+    "url": "/NewPortfolio/static/js/main.e4164218.chunk.js"
+  },
+  {
+    "revision": "ba46ff884b189b3ff0c6",
+    "url": "/NewPortfolio/static/js/runtime-main.078eeb51.js"
+  },
+  {
+    "revision": "2105de117ec27780e99c2d425d0d5844",
+    "url": "/NewPortfolio/static/media/CHINESER.2105de11.TTF"
+  },
+  {
+    "revision": "e7322a6673613ab13604fadda3d20e56",
+    "url": "/NewPortfolio/static/media/Valorant Font.e7322a66.ttf"
+  },
+  {
+    "revision": "1e09117b469515c9d690852ebfe97e02",
+    "url": "/NewPortfolio/static/media/ak.1e09117b.webp"
+  },
+  {
+    "revision": "a506b2e68c5b6662f597704a2516a02e",
+    "url": "/NewPortfolio/static/media/contactKeeper.a506b2e6.png"
+  },
+  {
+    "revision": "99e2a1e55eec6c4f41bcf93b312cd567",
+    "url": "/NewPortfolio/static/media/githubFinder.99e2a1e5.png"
+  },
+  {
+    "revision": "bef157a4ba4b5a4a04767de6c6519854",
+    "url": "/NewPortfolio/static/media/itlogger.bef157a4.png"
+  },
+  {
+    "revision": "8a8ec726271603e445706be0b73d4d31",
+    "url": "/NewPortfolio/static/media/memeswithak.8a8ec726.png"
+  },
+  {
+    "revision": "72282391cdd941455ea25b955d7ee035",
+    "url": "/NewPortfolio/static/media/portrait.72282391.jpg"
+  },
+  {
+    "revision": "2efd960d6f072c1970115149dc9607d6",
+    "url": "/NewPortfolio/static/media/project1.2efd960d.jpg"
+  },
+  {
+    "revision": "473d9f55d2f5045e41e3271c338cdb11",
+    "url": "/NewPortfolio/static/media/project2.473d9f55.jpg"
+  },
+  {
+    "revision": "139308d797d4a548920ce783be94237f",
+    "url": "/NewPortfolio/static/media/project3.139308d7.jpg"
+  },
+  {
+    "revision": "babc9580b3b97699fb8a21b526912d87",
+    "url": "/NewPortfolio/static/media/project5.babc9580.jpg"
+  },
+  {
+    "revision": "185e98b8e75c191db96cfc9bfb34aaa1",
+    "url": "/NewPortfolio/static/media/typingFinalcompressedagain.185e98b8.mp4"
+  },
+  {
+    "revision": "af7bf6687256813171dd6a22fd4f387c",
+    "url": "/NewPortfolio/static/media/violin.af7bf668.jpg"
+  }
+]);
